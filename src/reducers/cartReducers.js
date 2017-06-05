43d69b5e813d5cbe1fe507422261c6c8ca0cc2ad
hectorcoronado <coronado.hector@gmail.com>
2017-06-05 11:31:10 -0700

    calculates totalAmount & totalQty of cart & adds them to state

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -11,7 +11,12 @@ const INITIAL_STATE = {
 export default function cartReducers (state = INITIAL_STATE, action) {
   switch (action.type) {
     case ADD_TO_CART:
-      return { ...state, cart: action.payload }
+      return {
+        ...state,
+        cart: action.payload,
+        totalAmount: totals(action.payload).amount,
+        totalQty: totals(action.payload).qty
+      }
       break
 
     case UPDATE_CART:
@@ -31,14 +36,42 @@ export default function cartReducers (state = INITIAL_STATE, action) {
         ...currentProductToUpdate.slice(indexToUpdate + 1)
       ]
 
-      return { ...state, cart: cartUpdate }
+      return {
+        ...state,
+        cart: cartUpdate,
+        totalAmount: totals(cartUpdate).amount,
+        totalQty: totals(cartUpdate).qty
+      }
       break
 
     case DELETE_CART_ITEM:
-      return { ...state, cart: action.payload }
+      return {
+        ...state,
+        cart: action.payload,
+        totalAmount: totals(action.payload).amount,
+        totalQty: totals(action.payload).qty
+      }
       break
 
     default:
       return state
+  }
+}
+
+// same func used to calc totals for all cart reducers:
+export function totals (payloadArr) {
+  const totalAmount = payloadArr.map(
+    (cartArr) => { return cartArr.price * cartArr.quantity }
+    ).reduce(
+      (a, b) => { return a + b }, 0)
+
+  const totalQty = payloadArr.map(
+    (qty) => { return qty.quantity }
+    ).reduce(
+      (a, b) => { return a + b }, 0)
+
+  return {
+    amount: totalAmount.toFixed(2),
+    qty: totalQty
   }
 }
