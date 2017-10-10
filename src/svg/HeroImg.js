5b7c9ca3693ac79c55c742f44fed5d29e0610956
hectorcoronado <coronado.hector@gmail.com>
2017-10-09 19:01:51 -0700

    adds svg images

---
+++ src/svg/HeroImg.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const HeroImg = (props) => {
+  return (
+    <xml version='1.0' encoding='UTF-8' standalone='no'>
+      <svg width='330' height='330' viewBox='0 0 256 178' version='1.1'
+        xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink'>
+        <title>heroImage</title>
+        <desc>Created with Sketch.</desc>
+        <defs />
+        <g id='Page-1' stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
+          <g id='Letter' transform='translate(-121.000000, -420.000000)'>
+            <g id='heroImage' transform='translate(121.000000, 420.000000)'>
+              <g id='Group' transform='translate(0.000000, 78.000000)'>
+                <rect id='Rectangle-3' fill='#143D61' x='0' y='0' width='7' height='100' />
+                <rect id='Rectangle-3' fill='#34CDFF' x='7' y='0' width='7' height='100' />
+                <rect id='Rectangle-3' fill='#FFF7DD' x='14' y='0' width='7' height='100' />
+                <rect id='Red-2' fill='#CA181F' x='0' y='93' width='170.874384' height='6.93350142' />
+                <polygon id='Red-2' fill='#E6424D' points='0 86 170.874384 86 170.874384 92.9335014 74.8817294 92.9335014 0 92.9335014' />
+              </g>
+              <g id='Group-Copy' transform='translate(170.500000, 50.000000) scale(-1, -1) translate(-170.500000, -50.000000) translate(85.000000, 0.000000)'>
+                <rect id='Rectangle-3' fill='#143D61' x='0' y='0' width='7' height='100' />
+                <rect id='Rectangle-3' fill='#34CDFF' x='7' y='0' width='7' height='100' />
+                <rect id='Rectangle-3' fill='#FFF7DD' x='14' y='0' width='7' height='100' />
+                <rect id='Red-2' fill='#CA181F' x='0' y='93' width='170.874384' height='6.93350142' />
+                <polygon id='Red-2' fill='#E6424D' points='0 86 170.874384 86 170.874384 92.9335014 74.8817294 92.9335014 0 92.9335014' />
+              </g>
+              <text id='summvs' fontFamily='CutiveMono-Regular, Cutive Mono' fontSize='20' fontWeight='normal' letterSpacing='7' fill='#383838'>
+                <tspan x='81.671875' y='95'>summv</tspan>
+                <tspan x='177.21875' y='95'>s</tspan>
+              </text>
+            </g>
+          </g>
+        </g>
+      </svg>
+    </xml>
+  )
+}
+
+export default HeroImg
