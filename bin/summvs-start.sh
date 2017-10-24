d2dd87b2cd6e8484c92b5a232acd6dd7fdc37a2e
hectorcoronado <coronado.hector@gmail.com>
2017-10-23 17:12:57 -0700

    attempts to set PORT dynamically

--- bin/summvs-start.sh
+++ bin/summvs-start.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 # in the environment where heroku config:set BACK_END=true
-if [ $BACK_END == true ]
+if [ $FRONT_END == true ]
 then
-  node server.js
-  process.env.PORT == $PORT
-else
   ./bin/www
+  echo "Setting process.env.PORT=$PORT"
+  heroku config:set "PORT=$PORT" --app "summvsserver"
+else
+  node server.js
 fi
