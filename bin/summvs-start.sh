5910ae9284ff014fbc9d5c884681d0cc41425ddb
hectorcoronado <coronado.hector@gmail.com>
2017-10-24 10:38:14 -0700

    modifies script to set variable after builds

--- bin/summvs-start.sh
+++ bin/summvs-start.sh
@@ -4,7 +4,18 @@
 if [ $FRONT_END == true ]
 then
   ./bin/www
-  eval "heroku config:set PORT=$PORT --app summvsserver"
 else
   node server.js
 fi
+
+set -e
+
+sourceApp="$summvsclient"
+targetApp="$summvsserver"
+config=""
+
+while read line; do
+  config="$config $line"
+done  < <(heroku config --app "$sourceApp" --shell )
+
+eval "heroku config:set $config --app $targetApp"
