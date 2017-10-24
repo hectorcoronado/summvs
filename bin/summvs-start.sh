1f2828b4d049e29452ea503b5d1a63025b21190b
hectorcoronado <coronado.hector@gmail.com>
2017-10-23 17:39:08 -0700

    sets port with eval

--- bin/summvs-start.sh
+++ bin/summvs-start.sh
@@ -1,14 +1,10 @@
 #! /bin/bash
 
 # in the environment where heroku config:set BACK_END=true
-set -x
 if [ $FRONT_END == true ]
 then
-  echo "front end"
   ./bin/www
-  echo "Setting process.env.PORT=$PORT"
-  heroku config:set PORT=$PORT --app summvsserver
+  eval "heroku config:set PORT=$PORT --app summvsserver"
 else
-  echo "back end"
   node server.js
 fi
