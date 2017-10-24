d8d2a267e2d4fc1e20c65ed385a16923c643ca58
hectorcoronado <coronado.hector@gmail.com>
2017-10-23 17:23:31 -0700

    sets -x in bash script

--- bin/summvs-start.sh
+++ bin/summvs-start.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
 # in the environment where heroku config:set BACK_END=true
+set -x
 if [ $FRONT_END == true ]
 then
+  echo "front end"
   ./bin/www
   echo "Setting process.env.PORT=$PORT"
   heroku config:set "PORT=$PORT" --app "summvsserver"
 else
+  echo "back end"
   node server.js
 fi
