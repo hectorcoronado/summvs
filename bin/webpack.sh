ed1e465cd271b6ac373a3579c2c197e71b8c191f
hectorcoronado <coronado.hector@gmail.com>
2017-10-31 14:01:01 -0700

    starts webpack via heroku-postbuild script

--- bin/webpack.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# in the environmenbt where heroku config:set WEBPACK=prod
-if [ $WEBPACK == 'prod' ]
-then
-  webpack --config webpack.config.prod.js
-else
-  webpack --config webpack.config.dev.js --watch
-fi
