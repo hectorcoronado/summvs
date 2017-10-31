43f04466e0a1f4fede6709f8a957d657100af474
hectorcoronado <coronado.hector@gmail.com>
2017-10-30 17:34:27 -0700

    standardizes PurchaseComplete component

--- src/components/stripe/PurchaseComplete.js
+++ src/components/stripe/PurchaseComplete.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 class PurchaseComplete extends Component {
   render () {
     return (
-      <div>
-        thanks for your purchase.
+      <div className='row'>
+        <h6 className='col-xs-4 col-xs-offset-4 text-center'>
+          thank you for your purchase. you will receive email confirmation of your order shortly.
+        </h6>
       </div>
     )
   }
